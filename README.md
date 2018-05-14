b23a78bc9c6ddcf16b9465e6ee034a2e41d94a27
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2018-05-15 00:24:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# NAME
+
+WebService::Mocean - Perl library for integration with MoceanSMS gateway,
+https://dev.moceansms.com.
+
+# SYNOPSIS
+
+    use WebService::Mocean;
+
+# DESCRIPTION
+
+WebService::Mocean is Perl library for integration with MoceanSMS gateway,
+https://dev.moceansms.com.
+
+# COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2018 by Kian Meng, Ang.
+
+This is free software, licensed under:
+
+    The Artistic License 2.0 (GPL Compatible)
+
+# AUTHOR
+
+Kian Meng, Ang <kianmeng@users.noreply.github.com>
---
+++ lib/WebService/Mocean.pm
@@ -0,0 +1,36 @@
+package WebService::Mocean;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Mocean - Perl library for integration with MoceanSMS gateway,
+https://dev.moceansms.com.
+
+=head1 SYNOPSIS
+
+  use WebService::Mocean;
+
+=head1 DESCRIPTION
+
+WebService::Mocean is Perl library for integration with MoceanSMS gateway,
+https://dev.moceansms.com.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2018 by Kian Meng, Ang.
+
+This is free software, licensed under:
+
+    The Artistic License 2.0 (GPL Compatible)
+
+=head1 AUTHOR
+
+Kian Meng, Ang E<lt>kianmeng@users.noreply.github.comE<gt>
